003bc34979af0c7ff307692aa83d0d55a7a45c33
Jonshea Edwards <Jonshea23@gmail.com>
2024-10-02 23:52:33 -0400

    First Commit

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-#Function: This program determines if a student will be admitted or rejected.
-#Input:  Interactive
-#Output: Accept or Reject
+# Function: This program determines if a student will be admitted or rejected.
+# Input:  Interactive
+# Output: Accept or Reject
 
 # Get input and convert to correct data type for testScore and classRank
+testScoreString = input("Enter the student's test score: ")
+classRankString = input("Enter the student's class rank: ")
+
+# Convert string representation to integer
+testScore = int(testScoreString)
+classRank = int(classRankString)
 
 # Test using admission requirements and print Accept or Reject
 if testScore >= 90:
-  if classRank >= 25:
-    print("Accept")
-  else:
-    print("Reject")
-else:
-  if testScore >= 80:
-    if classRank >= 50:
-      print("Accept")
-    else:
-      print("Reject")
-  else:
-    if testScore >= 70:
-      if classRank >= 75:
+    if classRank >= 25:
         print("Accept")
-      else:
-        print("Reject")
     else:
-      print("Reject")
+        print("Reject")
+else:
+    if testScore >= 80:
+        if classRank >= 50:
+            print("Accept")
+        else:
+            print("Reject")
+    else:
+        if testScore >= 70:
+            if classRank >= 75:
+                print("Accept")
+            else:
+                print("Reject")
+        else:
+            print("Reject")
